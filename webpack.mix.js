f8910b1dd466d77f76a0ef819a5d86dd9015e3d8
Masahiro Fujioka <fujioka8700@gmail.com>
2022-07-02 20:52:08 +0900

    [add] ログインAPIのテスト

--- webpack.mix.js
+++ webpack.mix.js
@@ -16,14 +16,14 @@ mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .version()
     .browserSync({
-        proxy: {
-            target: 'apache2',
-        },
-        files: [
-            './resources/**/*',
-            './public/**/*',
-        ],
-        notify: false,
-        open: false,
-        reloadOnRestart: true,
+      proxy: {
+          target: 'apache2',
+      },
+      files: [
+          './resources/**/*',
+          './public/**/*',
+      ],
+      notify: false,
+      open: false,
+      reloadOnRestart: true,
     });
