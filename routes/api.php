1d57e982b0d68f54556bf6f48c2a7b14a093e0e2
Masahiro Fujioka <fujioka8700@gmail.com>
2022-07-02 21:12:56 +0900

    [add] ログアウトAPIのテスト

--- routes/api.php
+++ routes/api.php
@@ -22,3 +22,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/register', [RegisterController::class, 'register'])->name('register');
 Route::post('/login', [LoginController::class, 'login'])->name('login');
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
