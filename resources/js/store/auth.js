d389b665c520078bf5daf6aecf3ce8efd2686b76
Masahiro Fujioka <fujioka8700@gmail.com>
2022-07-08 10:50:16 +0900

    [add] システムエラーの処理を追加

--- resources/js/store/auth.js
+++ resources/js/store/auth.js
@@ -1,7 +1,9 @@
 import axios from "axios";
+import { OK } from '../util';
 
 const state = {
-  user: null
+  user: null,
+  apiStatus: null
 };
 
 const getters = {
@@ -12,6 +14,9 @@ const getters = {
 const mutations = {
   setUser(state, user) {
     state.user = user;
+  },
+  setApiStatus(state, status) {
+    state.apiStatus = status;
   }
 };
 
@@ -21,8 +26,17 @@ const actions = {
     context.commit('setUser', response.data);
   },
   async login(context, data) {
-    const response = await axios.post('/api/login', data);
-    context.commit('setUser', response.data);
+    const response = await axios.post('/api/login', data)
+      .catch(err => err.response || err);
+
+    if(response.status === OK) {
+      context.commit('setApiStatus', true);
+      context.commit('setUser', response.data);
+      return false;
+    }
+
+    context.commit('setApiStatus', false);
+    context.commit('error/setCode', response.status, { root: true });
   },
   async logout(context) {
     await axios.post('/api/logout');
