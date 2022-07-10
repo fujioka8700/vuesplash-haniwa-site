72a29265162fa52975e06f907699d63da7f4ce47
Masahiro Fujioka <fujioka8700@gmail.com>
2022-07-10 21:11:52 +0900

    [add] AWS S3 ファイルをアップロードするテスト

--- database/migrations/2022_07_10_080009_create_pictures_table.php
+++ database/migrations/2022_07_10_080009_create_pictures_table.php
@@ -14,7 +14,7 @@ class CreatePicturesTable extends Migration
     public function up()
     {
         Schema::create('pictures', function (Blueprint $table) {
-            $table->id();
+            $table->string('id')->primary();
             $table->unsignedBigInteger('user_id');
             $table->string('filename');
             $table->timestamps();
--- tests/Feature/PicturesSubmitApiTest.php
+++ tests/Feature/PicturesSubmitApiTest.php
@@ -2,9 +2,12 @@
 
 namespace Tests\Feature;
 
+use App\Models\Picture;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
 
 class PicturesSubmitApiTest extends TestCase
@@ -16,5 +19,31 @@ class PicturesSubmitApiTest extends TestCase
     parent::setUp();
 
     $this->user = User::factory()->create();
+  }
+
+  /**
+   * @test
+   */
+  public function should_ファイルをアップロードできる()
+  {
+    // S3ではなくテスト用のストレージを使用する
+    // storage/framework/testing/disks/s3/
+    Storage::fake('s3');
+
+    $response = $this->actingAs($this->user)->postJson(route('picture.create'), [
+      // ダミーファイルを作成して送信している
+      'picture' => UploadedFile::fake()->image('picture.jpg')
+    ]);
+
+    // レスポンスが201(CREATED)であること
+    $response->assertStatus(201);
+
+    $picture = Picture::first();
+
+    // 写真のIDが12桁のランダムな文字列であること
+    $this->assertMatchesRegularExpression('/^[0-9a-zA-Z-_]{12}$/', $picture->id);
+
+    // データベースに挿入されたファイル名のファイルが、ストレージに保存されていること
+    Storage::cloud()->assertExists($picture->filename);
   }
 }
