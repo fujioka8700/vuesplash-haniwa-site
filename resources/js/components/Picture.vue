ff34c5fa242a53d266bee6a932ffa0fc4736ab34
Masahiro Fujioka <fujioka8700@gmail.com>
2022-07-17 07:18:39 +0900

    [add] いいね、ダウンロードボタン、投稿者名追加

--- resources/js/components/Picture.vue
+++ resources/js/components/Picture.vue
@@ -1,12 +1,37 @@
 <template>
-  <div>
+  <div class="photo">
     <figure class="photo__wrapper">
       <img
-      class="photo__image"
+        class="photo__image"
         :src="item.url"
         :alt="`Picture by ${item.owner.name}`"
       >
     </figure>
+    <RouterLink
+      class="photo__overlay"
+      :to="`/pictures/${item.id}`"
+      :title="`View the picture by ${item.owner.name}`"
+    >
+      <div class="photo__controls">
+        <button
+          class="photo__action photo__action--like"
+          title="Like Picture"
+        >
+          <i class="icon ion-md-heart"></i>12
+        </button>
+        <a
+          class="photo__action"
+          title="Download Picture"
+          :href="`/pictures/${item.id}/download`"
+          @click.stop
+        >
+          <i class="icon ion-md-arrow-round-down"></i>
+        </a>
+      </div>
+      <div class="photo__username">
+        {{ item.owner.name }}
+      </div>
+    </RouterLink>
   </div>
 </template>
 
